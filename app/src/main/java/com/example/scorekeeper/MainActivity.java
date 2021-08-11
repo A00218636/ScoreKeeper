f245413bfd3bfd48ecd2e03ea3dbdc2ee1f1bf9a
A00218636 <A00218636@mycambrian.ca>
2021-08-11 11:09:03 -0700

    Added layouts for different orientations

--- app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+
+            //TO FADE THE VIEWS ON LOAD
             TextView tv = (TextView)findViewById(R.id.teamA);
             tv.setEnabled(false);
 
